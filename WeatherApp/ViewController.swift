b20533474bd3db6df3e55ced26c586010a7b8a9b
 Evgeniy <spiralcka@gmail.com>
2018-12-12 17:30:15 +0300

    initial commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by  Евгений on 12/12/2018.
+//  Copyright © 2018 LosAnatoly Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
